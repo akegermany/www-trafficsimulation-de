7e0a7f932726d74fa51bf2a61f7f4589979af279
ake <mail@akesting.de>
2012-11-07 20:56:47 +0100

    added license headers

--- src/main/java/de/trafficsimulation/core/IDMTruck.java
+++ src/main/java/de/trafficsimulation/core/IDMTruck.java
@@ -1,3 +1,27 @@
+/*
+ * Copyright (C) 2006-2012 by Martin Treiber, <treiber@vwi.tu-dresden.de>
+ * ---------------------------------------------------------------------------------
+ * 
+ * This file is part of the simulation project
+ * 
+ * www.traffic-simulation.de
+ * 
+ * It is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * The software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with MovSim. If not, see <http://www.gnu.org/licenses/>
+ * or <http://www.traffic-simulation.de>.
+ * 
+ * ---------------------------------------------------------------------------------
+ */
 package de.trafficsimulation.core;
 
 public class IDMTruck extends IDM implements MicroModel, Constants {
